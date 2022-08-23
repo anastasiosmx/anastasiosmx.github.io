09d5339afe7b24f5e52a6fb4887ab5dd4d80a204
Anastasios Mexias <a.mexias@lbsuite.eu>
2022-08-23 17:10:12 +0300

    Changed title header to typewritter effect

--- src/Header/header-name.tsx
+++ src/Header/header-name.tsx
@@ -1,16 +1,22 @@
 import { HeaderJobTitle } from "./header-job-title";
+import Typewriter from 'typewriter-effect';
 import "./header-name.css";
 
 export const HeaderName = () => {
     return(
         <>
             <div className="header-full-name">
-                <h1 className="header-bg">
-                    <span className="header-gt">
-                        &gt;
-                    </span>
-                    Anastasios Mexias
-                </h1>
+                <span className="header-gt">
+                    &gt;
+                </span>
+                <Typewriter
+                    options={{
+                        strings: '<span class="header-name">Anastasios Mexias</span>',
+                        autoStart: true,
+                        delay: 100,
+                        cursorClassName: "cursor-class"
+                    }}
+                />
             </div>
             <HeaderJobTitle />
         </>
