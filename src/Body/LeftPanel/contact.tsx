fd87da95a0bf1e8faec60f247650c23513ae53eb
Anastasios Mexias <a.mexias@lbsuite.eu>
2022-08-29 10:41:21 +0300

    Obfuscated contact details

--- src/Body/LeftPanel/contact.tsx
+++ src/Body/LeftPanel/contact.tsx
@@ -1,4 +1,5 @@
 import "./contact.css";
+import { Obfuscate } from '@south-paw/react-obfuscate-ts';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPhone, faEnvelope, faLocationDot } from '@fortawesome/free-solid-svg-icons'
 import { faGitlab, faGithub } from "@fortawesome/free-brands-svg-icons";
@@ -8,23 +9,22 @@ export const Contact = () => {
         <>
         <h1 className="letters-spacing"><span className="cursor">&gt;</span>&nbsp;CONTACT</h1> 
         <div className="contact-label">
-            <a href="tel:+306979708828">
-                <FontAwesomeIcon icon={faPhone} fixedWidth size="lg" color="#ff914d" />
-                <span className="contact-text letters-spacing">
-                    +30 697-970-8828
-                </span>
-            </a>
+            <FontAwesomeIcon icon={faPhone} fixedWidth size="lg" color="#ff914d" />
+            <span className="contact-text letters-spacing">
+                <Obfuscate tel>
+                    [GR] 697-970-8828
+                </Obfuscate>
+            </span>
         </div>
         <div className="contact-label">
-            <a href="mailto:mxanastasios@gmail.com" target="_blank" rel="noreferrer">
-                <FontAwesomeIcon icon={faEnvelope} fixedWidth size="lg" color="#ff914d" />
-                <span className="contact-text letters-spacing">
-                    <span className="orange-text">
-                        mxanastasios    
-                    </span> 
-                    @gmail.com
+            <FontAwesomeIcon icon={faEnvelope} fixedWidth size="lg" color="#ff914d" />
+            <span className="contact-text letters-spacing">
+                <span className="orange-text">
+                <Obfuscate email>
+                    mxanastasios@gmail.com
+                </Obfuscate>
                 </span>
-            </a>
+            </span>
         </div>
         <div className="contact-label">
             <FontAwesomeIcon icon={faLocationDot} fixedWidth size="lg" color="#ff914d" />
@@ -33,26 +33,20 @@ export const Contact = () => {
             </span>
         </div>
         <div className="contact-label">
-            <a href="https://gitlab.com/a.mexias" target="_blank" rel="noreferrer">
-                <FontAwesomeIcon icon={faGitlab} fixedWidth size="lg" color="#ff914d" />
-                <span className="contact-text letters-spacing">
-                    gitlab.com/
-                    <span className="orange-text">
-                        a.mexias    
-                    </span> 
-                </span>
-            </a>
+            <FontAwesomeIcon icon={faGitlab} fixedWidth size="lg" color="#ff914d" />
+            <span className="contact-text letters-spacing">
+                <Obfuscate href="https://gitlab.com/a.mexias">
+                    gitlab.com/a.mexias
+                </Obfuscate>
+            </span>
         </div>
         <div className="contact-label">
-            <a href="https://github.com/anastasiosmx" target="_blanc" rel="noreferrer">
-                <FontAwesomeIcon icon={faGithub} fixedWidth size="lg" color="#ff914d" />
-                <span className="contact-text letters-spacing">
-                    github.com/
-                    <span className="orange-text">
-                        anastasiosmx    
-                    </span> 
-                </span>
-            </a>
+            <FontAwesomeIcon icon={faGithub} fixedWidth size="lg" color="#ff914d" />
+            <span className="contact-text letters-spacing">
+                <Obfuscate href="https://github.com/anastasiosmx">
+                    github.com/anastasiosmx
+                </Obfuscate>
+            </span>
         </div>
         </> 
     );
