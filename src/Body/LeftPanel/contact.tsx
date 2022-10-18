2f857bdc873e92cd01b46d643dcecb37de1ee77c
Anastasios Mexias <a.mexias@lbsuite.eu>
2022-10-18 20:48:33 +0300

    Refactor: changed fractional precentage fro left panel to 1. Feat: Added logic for opening and closing categories

--- src/Body/LeftPanel/contact.tsx
+++ src/Body/LeftPanel/contact.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import "./contact.css";
 import { Obfuscate } from '@south-paw/react-obfuscate-ts';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -5,6 +6,8 @@ import { faEnvelope, faLocationDot } from '@fortawesome/free-solid-svg-icons'
 import { faGitlab, faGithub } from "@fortawesome/free-brands-svg-icons";
 
 export const Contact = () => {
+    const [showContactInfo, setShowContactInfo] = React.useState(true);
+
     const gitLabDomain: string = "mexias";
     const gitLabUrl:    string = "https://gitlab.com/a.mexias";
     const gitHubDomain: string = "anastasiosmx";
@@ -12,55 +15,66 @@ export const Contact = () => {
     const emailPrefix:  string = "mx";
     const emailBody:    string = "anastasios";
 
+
+    const contactVisibilityHandler = () => {
+        setShowContactInfo(!showContactInfo);
+    }
+
     return(
         <>
-        <h1 className="letters-spacing"><span className="cursor">&gt;</span>&nbsp;CONTACT</h1> 
-        <div className="contact-label">
-            <FontAwesomeIcon icon={faEnvelope} fixedWidth size="lg" color="#ff914d" />
-            <span className="contact-text letters-spacing">
-                <span className="orange-text">
-                <Obfuscate email>
-                    {emailPrefix + emailBody + "@gmail.com"}
-                </Obfuscate>
+            <h1 className="letters-spacing"><span className="cursor" onClick={contactVisibilityHandler}>&gt;</span>&nbsp;CONTACT</h1>
+            {showContactInfo? 
+                <span> 
+                    <div className="contact-label">
+                        <FontAwesomeIcon icon={faEnvelope} fixedWidth size="lg" color="#ff914d" />
+                        <span className="contact-text letters-spacing">
+                            <span className="orange-text">
+                            <Obfuscate email>
+                                {emailPrefix + emailBody + "@gmail.com"}
+                            </Obfuscate>
+                            </span>
+                        </span>
+                    </div>
+                    <div className="contact-label">
+                        <FontAwesomeIcon icon={faLocationDot} fixedWidth size="lg" color="#ff914d" />
+                        <span className="contact-text letters-spacing">
+                            Thessaloniki, Greece
+                        </span>
+                    </div>
+                    <div className="contact-label">
+                        <FontAwesomeIcon icon={faGitlab} fixedWidth size="lg" color="#ff914d" />
+                        <span className="contact-text letters-spacing">
+                            <Obfuscate href={gitLabUrl}>
+                                gitlab.com/
+                            </Obfuscate>
+                        </span>
+                        <span className="letters-spacing">
+                            <span className="orange-text">
+                                a
+                            </span>
+                            .
+                            <span className="orange-text">
+                                {gitLabDomain}
+                            </span>
+                        </span>
+                    </div>
+                    <div className="contact-label">
+                        <FontAwesomeIcon icon={faGithub} fixedWidth size="lg" color="#ff914d" />
+                        <span className="contact-text letters-spacing">
+                            <Obfuscate href={gitHubUrl}>
+                                github.com/
+                            </Obfuscate>
+                        </span>
+                        <span className="letters-spacing">
+                            <span className="orange-text">
+                                {gitHubDomain}
+                            </span>
+                        </span>
+                    </div>
                 </span>
-            </span>
-        </div>
-        <div className="contact-label">
-            <FontAwesomeIcon icon={faLocationDot} fixedWidth size="lg" color="#ff914d" />
-            <span className="contact-text letters-spacing">
-                Thessaloniki, Greece
-            </span>
-        </div>
-        <div className="contact-label">
-            <FontAwesomeIcon icon={faGitlab} fixedWidth size="lg" color="#ff914d" />
-            <span className="contact-text letters-spacing">
-                <Obfuscate href={gitLabUrl}>
-                    gitlab.com/
-                </Obfuscate>
-            </span>
-            <span className="letters-spacing">
-                <span className="orange-text">
-                    a
-                </span>
-                .
-                <span className="orange-text">
-                    {gitLabDomain}
-                </span>
-            </span>
-        </div>
-        <div className="contact-label">
-            <FontAwesomeIcon icon={faGithub} fixedWidth size="lg" color="#ff914d" />
-            <span className="contact-text letters-spacing">
-                <Obfuscate href={gitHubUrl}>
-                    github.com/
-                </Obfuscate>
-            </span>
-            <span className="letters-spacing">
-                <span className="orange-text">
-                    {gitHubDomain}
-                </span>
-            </span>
-        </div>
+                :
+                null
+            }
         </> 
     );
 };
--- src/Body/LeftPanel/skills.tsx
+++ src/Body/LeftPanel/skills.tsx
@@ -1,78 +1,91 @@
+import React from "react";
 import "./skills.css";
 import { SkillsRating } from "./skillsRating";
 
 export const Skills = () => {
+    const [showSkills, setShowSkills] = React.useState(true);
+
+    const skillsVisibilityHandler = () => {
+        setShowSkills(!showSkills);
+    }
+
     return(
         <>
-            <h1 className="letters-spacing"><span className="cursor">&gt;</span>&nbsp;SKILLS</h1> 
-            <table className="letters-spacing">
-                <tbody>
-                    <tr>
-                        <td>
-                            &#8226; HTML, CSS
-                        </td>
-                        <td>
-                            <SkillsRating rating={5} />
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            &#8226; JavaScript (ES6)
-                        </td>
-                        <td>
-                            <SkillsRating rating={4} />
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            &#8226; TypeScript
-                        </td>
-                        <td>
-                            <SkillsRating rating={4} />
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            &#8226; PhP
-                        </td>
-                        <td>
-                            <SkillsRating rating={4} />
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            &#8226; Java
-                        </td>
-                        <td>
-                            <SkillsRating rating={3} />
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            &#8226; C
-                        </td>
-                        <td>
-                            <SkillsRating rating={3} />
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            &#8226; Python
-                        </td>
-                        <td>
-                            <SkillsRating rating={2} />
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            &#8226; MySQL
-                        </td>
-                        <td>
-                            <SkillsRating rating={2} />
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+            <h1 className="letters-spacing"><span className="cursor" onClick={skillsVisibilityHandler}>&gt;</span>&nbsp;SKILLS</h1> 
+            {showSkills? 
+                <span>
+                    <table className="letters-spacing">
+                        <tbody>
+                            <tr>
+                                <td>
+                                    &#8226; HTML, CSS
+                                </td>
+                                <td>
+                                    <SkillsRating rating={5} />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    &#8226; JavaScript (ES6)
+                                </td>
+                                <td>
+                                    <SkillsRating rating={4} />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    &#8226; TypeScript
+                                </td>
+                                <td>
+                                    <SkillsRating rating={4} />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    &#8226; PhP
+                                </td>
+                                <td>
+                                    <SkillsRating rating={4} />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    &#8226; Java
+                                </td>
+                                <td>
+                                    <SkillsRating rating={3} />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    &#8226; C
+                                </td>
+                                <td>
+                                    <SkillsRating rating={3} />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    &#8226; Python
+                                </td>
+                                <td>
+                                    <SkillsRating rating={2} />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    &#8226; MySQL
+                                </td>
+                                <td>
+                                    <SkillsRating rating={2} />
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </span>
+                :
+                null
+            }
         </>
     );
 };
