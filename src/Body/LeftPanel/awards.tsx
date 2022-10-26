e70827404ec80fef9c4146feb309a2ac8520be32
Anastasios Mexias <a.mexias@lbsuite.eu>
2022-10-26 19:06:29 +0300

    Refactor: Added open-close functionality to component & finalized content

--- src/Body/LeftPanel/awards.tsx
+++ src/Body/LeftPanel/awards.tsx
@@ -1,7 +1,26 @@
+import React from "react";
+import './awards.css';
+
 export const Awards = () => {
+    const [showAwards, setshowAwards] = React.useState(true);
+
+    const awardsVisibilityHandler = () => {
+        setshowAwards(!showAwards);
+    }
+
     return(
         <>
-            <h1>Hello</h1>
+            <h1 className="letters-spacing"><span className="cursor " onClick={awardsVisibilityHandler}>&gt;</span>&nbsp;AWARDS</h1> 
+            {showAwards?
+                <>
+                    <h3 className="paragraph-title-deep-purple">IEEE REGION 8 EXEMPLARY STUDENT BRANCH AWARD</h3>
+                    <p>
+                        IEEE Student Branch University of Thessaly <span className="orange bold">|</span> YEARS: 2017 - 2019
+                    </p>
+                </>
+                :
+                null
+            }
         </>
     );
 }
