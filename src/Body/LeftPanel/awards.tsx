ef610f9ca9bffe2f28b41fc7a970ce53048f8d35
Anastasios Mexias <a.mexias@lbsuite.eu>
2022-10-26 18:49:54 +0300

    Feat: Initialized Awards component and integrated it into left panel

---
+++ src/Body/LeftPanel/awards.tsx
@@ -0,0 +1,7 @@
+export const Awards = () => {
+    return(
+        <>
+            <h1>Hello</h1>
+        </>
+    );
+}
--- src/Body/LeftPanel/leftPanel.tsx
+++ src/Body/LeftPanel/leftPanel.tsx
@@ -1,6 +1,7 @@
 import { Contact } from "./contact";
 import { Skills } from "./skills";
 import { Frameworks } from "./frameworks";
+import { Awards } from "./awards";
 
 export const LeftPanel = () => {
     return(
@@ -8,6 +9,7 @@ export const LeftPanel = () => {
             <Contact />
             <Skills />
             <Frameworks />
+            <Awards />
         </>
     );
 };
