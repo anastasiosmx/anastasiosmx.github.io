3dffa01d79f684edfef0d7d393f6e1a789b4f3e1
Anastasios Mexias <a.mexias@lbsuite.eu>
2022-11-02 13:12:21 +0200

    Refactor (education): Added content & cursor utility

--- src/Body/RightPanel/education.tsx
+++ src/Body/RightPanel/education.tsx
@@ -1,7 +1,22 @@
+import React from "react";
+
 export const Education = () => {
+    const [showEducationInfo, setShowEducationInfo] = React.useState(true);
+
+    const educationVisibilityHandler = () => {
+        setShowEducationInfo(!showEducationInfo);
+    }
+
     return(
         <>
-            <h1>test</h1>
+            <h1 className="letters-spacing"><span className="cursor" onClick={educationVisibilityHandler}>&gt;</span>&nbsp;EDUCATION</h1>
+            {showEducationInfo?
+                <span className="justify-text-full letters-spacing">
+                    
+                </span>
+                :
+                null
+            }
         </>
     );
 }
