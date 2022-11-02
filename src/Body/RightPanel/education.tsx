320194b724e3f8b7d6d7edb7acf24b56d6356f5c
Anastasios Mexias <a.mexias@lbsuite.eu>
2022-11-02 13:25:34 +0200

    Refactor (education): Added content

--- src/Body/RightPanel/education.tsx
+++ src/Body/RightPanel/education.tsx
@@ -11,8 +11,42 @@ export const Education = () => {
         <>
             <h1 className="letters-spacing"><span className="cursor" onClick={educationVisibilityHandler}>&gt;</span>&nbsp;EDUCATION</h1>
             {showEducationInfo?
-                <span className="justify-text-full letters-spacing">
-                    
+                <span className="letters-spacing">
+                    <h3 className="paragraph-title-deep-purple">BSC OF COMPUTER SCIENCE & BIOMEDICAL INFORMATICS</h3>
+                    <p>
+                        UNIVERSITY OF THESSALY &nbsp;
+                        <span className="orange bold">
+                            |
+                        </span>
+                        &nbsp;2015&nbsp;
+                        <span className="orange bold">
+                            -
+                        </span>
+                        &nbsp;2020
+                        <br/>
+                        <span className="orange bold">
+                            GPA
+                        </span>
+                        &nbsp;: 7.57 / 10
+                    </p>
+
+                    <h3 className="paragraph-title-deep-purple">ECCE <span className="orange bold">|</span> ENGLISH LANGUAGE</h3>
+                    <p>
+                        UNIVERSITY OF MICHIGAN &nbsp;
+                        <span className="orange bold">
+                            |
+                        </span>
+                        &nbsp;2012
+                    </p>
+
+                    <h3 className="paragraph-title-deep-purple">ECPE <span className="orange bold">|</span> ENGLISH LANGUAGE</h3>
+                    <p>
+                        UNIVERSITY OF MICHIGAN &nbsp;
+                        <span className="orange bold">
+                            |
+                        </span>
+                        &nbsp;2022
+                    </p>
                 </span>
                 :
                 null
