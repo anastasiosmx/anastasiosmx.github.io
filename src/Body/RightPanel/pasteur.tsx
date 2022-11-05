07428ad8903a8c344e194877482f8c79f5d5de5b
Anastasios Mexias <a.mexias@lbsuite.eu>
2022-11-06 00:01:40 +0200

    Feat (pasteur): Created & integrsted pasteur component

---
+++ src/Body/RightPanel/pasteur.tsx
@@ -0,0 +1,38 @@
+export const Pasteur = () => {
+    return(
+        <>
+            <span className="letters-spacing">
+                <h3 className="paragraph-title-deep-purple">INTERNSHIP STUDENT</h3>
+                <p>
+                    HELLENIC PASTEUR INSTITUTE&nbsp;
+                    <span className="orange bold">
+                        |
+                    </span>
+                    &nbsp;JUN 2019&nbsp;
+                    <span className="orange bold">
+                        -
+                    </span>
+                    &nbsp;JAN 2022
+                    <br/>
+                </p>
+                <ul>
+                    <li className="justify-text-full letters-spacing">
+                        Analysis of next-generation sequencing
+                        data (NGS-RNAseq) & micro-array data
+                        using bioinformatics ML tools and
+                        custom scripts written in Python & R.
+                    </li>
+                    <li className="letters-spacing">
+                        1 Publication&nbsp;
+                        <span className="orange bold">|</span>&nbsp;
+                        https://doi.org/10.1101/2020.03.27.009480
+                    </li>
+                    <li className="justify-text-full letters-spacing">
+                        Various presentations in conferences in
+                        Greece about bioinformatics and genomics
+                    </li>
+                </ul>
+            </span>
+        </>
+    );
+}
