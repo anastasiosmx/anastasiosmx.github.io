ba07b82fcea4535b63a881caa408410e0b8f9ef4
Anastasios Mexias <a.mexias@lbsuite.eu>
2022-11-05 23:21:21 +0200

    Feat (LinkedBusiness): Seperated component content for LinkedBUsiness

---
+++ src/Body/RightPanel/linkedBusiness.tsx
@@ -0,0 +1,88 @@
+export const LinkedBusiness = () => {
+    return(
+        <>
+            <span className="letters-spacing">
+                <h3 className="paragraph-title-deep-purple">FRONTEND DEVELOPER</h3>
+                <p>
+                    LINKED BUSINESS&nbsp;
+                    <span className="orange bold">
+                        |
+                    </span>
+                    &nbsp;JAN 2022&nbsp;
+                    <span className="orange bold">
+                        -
+                    </span>
+                    &nbsp;Today
+                    <br/>
+                </p>
+                <ul>
+                    <li className="justify-text-full letters-spacing">
+                        Created from scratch a platform that offered
+                        information for all kind of elderly nursing
+                        homes in greece using Google Maps API.
+                        Further functionality was added later with
+                        admin panel page and options to add and
+                        view multiple different maps throught API
+                        requests.
+                        <ul>
+                            <br/>
+                            <li>
+                                <span className="orange bold">
+                                    Technologies used
+                                </span>
+                                : Angular, ExpresJs, Bootstrap and MongoDB
+                            </li>
+                            <br/>
+                        </ul>
+                    </li>
+                    <li className="justify-text-full letters-spacing">
+                        Participated in the creation of a Business
+                        Live Report that is being created
+                        automatically in demand. The Live Report
+                        provides usefull economical insight for the
+                        specified company in a form of a pdf file.
+                        <ul>
+                            <br/>
+                            <li>
+                                <span className="orange bold">
+                                    Technologies used
+                                </span>
+                                : Python, Jinja, HTML, CSS, Bootstrap
+                            </li>
+                            <br/>
+                        </ul>
+                    </li>
+                    <li className="justify-text-full letters-spacing">
+                        Created & delivered to customer API endpoint that 
+                        retrieves all hair salons that were created in 
+                        Greece for a specific month / year
+                        <ul>
+                            <br/>
+                            <li>
+                                <span className="orange bold">
+                                    Technologies used
+                                </span>
+                                : Laravell, php
+                            </li>
+                            <br/>
+                        </ul>
+                    </li>
+                    <li className="justify-text-full letters-spacing">
+                        Participated in the maintainance & further development 
+                        of all web apllication of the company
+                        <ul>
+                            <br/>
+                            <li>
+                                <span className="orange bold">
+                                    Technologies used
+                                </span>
+                                : Angular, React, Laravel, TypeScript, PhP 
+                            </li>
+                            <br/>
+                        </ul>
+                    </li>
+                </ul>
+            </span>
+        </>
+    );
+}
