91d7f7c1bc5716b01dae649c72f8369454c5027a
Anastasios Mexias <a.mexias@lbsuite.eu>
2022-11-05 22:42:32 +0200

    Feat (RightPanel): Integrated experience component

--- src/Body/RightPanel/rightPanel.tsx
+++ src/Body/RightPanel/rightPanel.tsx
@@ -1,11 +1,13 @@
 import { Profile } from "./profile";
 import { Education } from "./education";
+import { Experience } from "./experience";
 
 export const RightPanel = () => {
     return(
         <>
             <Profile />
             <Education />
+            <Experience />
         </>
     );
 };
