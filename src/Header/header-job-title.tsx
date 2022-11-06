f3016bcaf2862a668c6f739fb3e5d715c585b9da
Anastasios Mexias <a.mexias@lbsuite.eu>
2022-11-06 17:52:46 +0200

    Feat: Added download as PDF button

--- src/Header/header-job-title.tsx
+++ src/Header/header-job-title.tsx
@@ -1,12 +1,24 @@
 import "./header-job-title.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFileArrowDown } from '@fortawesome/free-solid-svg-icons'
 
 export const HeaderJobTitle = () => {
     return(
-        <div className="job-title">
-            <span className="job-specialty">
-                FRONTEND
-            </span>&nbsp;
-            DEVELOPER
-        </div>
+        <>
+            <div className="job-title">
+                <span className="job-specialty">
+                    FRONTEND
+                </span>&nbsp;
+                DEVELOPER
+            </div>
+            <span className="download-icon visibility-hint">
+                <span className="print-button-descr">
+                    Print me!
+                </span>
+                <button className="download-button" onClick={() => window.print()}>
+                    <FontAwesomeIcon icon={faFileArrowDown} fixedWidth size="lg" color="#ff914d" />
+                </button>
+            </span>
+        </>
     );
 };
